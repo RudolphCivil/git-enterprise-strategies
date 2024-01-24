09aaf1b419a33656ba2d46270c986b7d847bcb81
Civil, Rudolph <civilrudolph@gmail.com>
2024-01-23 23:36:02 -0500

    Meaningful commit message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
+import gator from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<<<<<<< Updated upstream
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
+	<header className-"App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Rudolph Civil and this is my project </p>
->>>>>>> Stashed changes
       </header>
     </div>
   );
